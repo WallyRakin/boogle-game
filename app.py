4fe57c2a5b0e591d740f118912415b237f2fa4f8
WallyRakin <83788022+WallyRakin@users.noreply.github.com>
2023-07-13 13:36:50 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -11,21 +11,18 @@ app.secret_key = 'your_secret_key_here'
 @app.route("/")
 def home():
     # creates a new session if a valid session is not already available
-    if not (session['data'] and session['end-time'] and session['valid-words'] and session['found-words'] and session['score']) or (datetime.now() > datetime.strptime(session.get('end-time'), "%Y-%m-%d %H:%M:%S")):
+    if not (session.get('data') and session.get('end-time') and session.get('found-words') and session.get('score')) or (datetime.now() > datetime.strptime(session.get('end-time'), "%Y-%m-%d %H:%M:%S")):
         board = boggle_game.make_board()
         session['data'] = json.dumps(board)
         session['end-time'] = ((datetime.now() +
                                 timedelta(minutes=2)).strftime('%Y-%m-%d %H:%M:%S'))
-        session['valid-words'] = '[]'
-        # session['invalid-words'] = '[]'
         session['found-words'] = '[]'
         session['score'] = '0'
         # print(json.dumps(board))
     else:
         board = json.loads(session.get('data'))
     found_words = json.loads(session.get('found-words'))
-    score = int(session.get('score'))
-    return render_template('home.html', board=board, row=range(len(board)), col=range(len(board[0])), data=json.dumps(board), found_words=found_words, score=score)
+    return render_template('home.html', board=board, row=range(len(board)), col=range(len(board[0])), data=json.dumps(board), found_words=found_words)
 
 
 @app.route("/game", methods=['GET', 'POST'])
@@ -40,7 +37,7 @@ def game():
             json.loads(session.get('data')), word)
 
         found_words = json.loads(session.get('found-words'))
-        print(found_words)
+        # print(found_words)
 
         if datetime.now() > datetime.strptime(session.get('end-time'), "%Y-%m-%d %H:%M:%S"):
             # escapes function if the game is already over
@@ -53,14 +50,8 @@ def game():
         score = int(session.get('score'))
 
         if result == 'ok':
-            valid_words = json.loads(session.get('valid-words'))
-            valid_words.append(result)
-            session['valid-words'] = json.dumps(valid_words)
             score += 100
         elif result == 'not-on-board':
-            # invalid_words = json.loads(session.get('invalid-words'))
-            # invalid_words.append(result)
-            # session['invalid-words'] = json.dumps(invalid_words)
             return result
         else:
             return result
@@ -73,4 +64,4 @@ def game():
 
 
 # Ensure this is set to True or not set at all
-app.run(debug=True)
+# app.run(debug=True)
