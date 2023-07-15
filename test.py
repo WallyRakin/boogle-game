2f5aeaa8ed921bcecf446a656bd5855a0c60b82a
WallyRakin <83788022+WallyRakin@users.noreply.github.com>
2023-07-15 01:58:28 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -6,11 +6,60 @@ from datetime import datetime, timedelta
 import json
 
 
-class FlaskTests(unittest.TestCase):
+class boggle_tests(unittest.TestCase):
+
+    def setUp(self):
+        self.boggle_game = Boggle()
+
+    def test_read_dict(self):
+        self.assertEqual(type(self.boggle_game.read_dict("words.txt")), list)
+
+    def test_make_baord(self):
+        board = self.boggle_game.make_board()
+        self.assertEqual(len(board), 5)
+        for row in board:
+            self.assertEqual(type(row), list)
+            self.assertEqual(len(row), 5)
+            for obj in row:
+                self.assertEqual(type(obj), str)
+                self.assertEqual(len(obj), 1)
+                self.assertIn(obj, [chr(i)
+                              for i in range(ord('A'), ord('Z')+1)])
+
+    def test_check_valid_word(self):
+        board = [["Y", "Y", "Z", "I", "H"], ["Y", "H", "C", "N", "G"], [
+            "G", "V", "S", "S", "M"], ["F", "N", "B", "U", "I"], ["P", "M", "J", "X", "V"]]
+        self.assertEqual(self.boggle_game.check_valid_word(board, 'bus'), 'ok')
+        self.assertEqual(self.boggle_game.check_valid_word(
+            board, 'apple'), 'not-on-board')
+        self.assertEqual(self.boggle_game.check_valid_word(
+            board, 'yyzih'), 'not-word')
+        self.assertEqual(self.boggle_game.check_valid_word(
+            board, 'bu'), 'word is too short')
+
+    def test_find_from(self):
+        board = [["Y", "Y", "Z", "I", "H"], ["Y", "H", "C", "N", "G"], [
+            "G", "V", "S", "S", "M"], ["F", "N", "B", "U", "I"], ["P", "M", "J", "X", "V"]]
+        self.assertTrue(self.boggle_game.find_from(
+            board, 'ZINC', 0, 2, seen=set()))
+        self.assertTrue(self.boggle_game.find_from(
+            board, 'BUS', 3, 2, seen=set()))
+        self.assertFalse(self.boggle_game.find_from(
+            board, 'ZINC', 0, 0, seen=set()))
+        self.assertFalse(self.boggle_game.find_from(
+            board, 'BUS', 0, 0, seen=set()))
+
+    def test_find(self):
+        board = [["Y", "Y", "Z", "I", "H"], ["Y", "H", "C", "N", "G"], [
+            "G", "V", "S", "S", "M"], ["F", "N", "B", "U", "I"], ["P", "M", "J", "X", "V"]]
+        self.assertTrue(self.boggle_game.find(board, 'BUS'))
+        self.assertTrue(self.boggle_game.find(board, 'ZINC'))
+
+
+class blask_tests(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
-        self.boggle_game = Boggle()
 
     def test_home(self):
         with self.app.session_transaction() as session:
@@ -18,7 +67,7 @@ class FlaskTests(unittest.TestCase):
             session['end-time'] = str((datetime.now() +
                                        timedelta(minutes=2)).strftime('%Y-%m-%d %H:%M:%S'))
             session['score'] = '0'
-        session['found-words'] = '[]'
+            session['found-words'] = '[]'
         response = self.app.get()
 
         self.assertEqual(response.status_code, 200)
@@ -59,6 +108,10 @@ class FlaskTests(unittest.TestCase):
         response = self.app.post('/game', json={'word': 'fnbui'})
         self.assertNotEqual(b'ok', response.data)
 
+        # Test word that is too short
+        response = self.app.post('/game', json={'word': 'fn'})
+        self.assertNotEqual(b'ok', response.data)
+
         # Test valid word that doesn't exist
         response = self.app.post('/game', json={'word': 'apple'})
         self.assertNotEqual(b'ok', response.data)
@@ -67,13 +120,24 @@ class FlaskTests(unittest.TestCase):
             session['data'] = '[["Y", "Y", "Z", "I", "H"], ["Y", "H", "C", "N", "G"], ["G", "V", "S", "S", "M"], ["F", "N", "B", "U", "I"], ["P", "M", "J", "X", "V"]]'
             session['end-time'] = str((datetime.now() -
                                        timedelta(minutes=2)).strftime('%Y-%m-%d %H:%M:%S'))  # Set time to the past to simulate a timed-out game
-            session['score'] = '0'
-            session['found-words'] = '[]'
+            session['score'] = '100'
+            session['found-words'] = json.dumps(['bus'])
 
             # Test valid word after timed out
         response = self.app.post('/game', json={'word': 'zinc'})
         self.assertEqual(
             b"can't add anymore words, game is already over", response.data)
+
+        with self.app.session_transaction() as session:
+            session['data'] = '[["Y", "Y", "Z", "I", "H"], ["Y", "H", "C", "N", "G"], ["G", "V", "S", "S", "M"], ["F", "N", "B", "U", "I"], ["P", "M", "J", "X", "V"]]'
+            session['end-time'] = str((datetime.now() +
+                                       timedelta(minutes=2)).strftime('%Y-%m-%d %H:%M:%S'))  # Set time to the past to simulate a timed-out game
+            session['score'] = '100'
+            session['found-words'] = json.dumps(['bus'])
+
+        response = self.app.post('/game', json={'word': 'bus'})
+        self.assertEqual(
+            b'word has already been selected', response.data)
 
 
 if __name__ == '__main__':
